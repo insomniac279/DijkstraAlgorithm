19a704c1981356b76da8821d80d43436f5b14c12
Ezra <ezra.zogg@gmail.com>
2023-10-23 22:35:58 +0200

    First version of Dikstra Algorithm

---
+++ DijkstraAlgorithm/GraphWithNodes.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+
+namespace DijkstraAlgorithm
+{
+    public class GraphWithNodes
+    {
+        public List<(char startNode, char endNode, int cost)> givenPaths;
+
+        public GraphWithNodes(List<(char startNode, char endNode, int cost)> givenPaths)
+        {
+            this.givenPaths = givenPaths;
+        }
+
+
+        public Dictionary<char, int> CalculateMinimalCostFromNode(char selectedNode)
+        {
+            var allNodes = new HashSet<char>();
+
+            foreach (var (startNode, endNode, cost) in givenPaths)
+            {
+                allNodes.Add(startNode);
+                allNodes.Add(endNode);
+            }
+
+            allNodes.Remove(selectedNode);
+
+            var minimalPathsFromNode = new Dictionary<char, int>();
+
+            //Initialize costs to each node with given paths
+            foreach (var node in allNodes)
+            {
+                if (CheckIfDirectPathExists(selectedNode, node))
+                {
+                    minimalPathsFromNode.Add(node, GetDirectPathCost(selectedNode, node));
+                }
+
+                else
+                {
+                    minimalPathsFromNode.Add(node, int.MaxValue);
+                }
+            }
+
+            //for each node that isn't the start node, see if there is a better way
+
+            var currentMinimalCost = minimalPathsFromNode.Where(n => allNodes.Contains(n.Key)).Min(n => n.Value);
+            var layoverNode = minimalPathsFromNode.Where(n => allNodes.Contains(n.Key) && n.Value == currentMinimalCost).Select(n => n.Key).FirstOrDefault();
+            var allRemainingNodes = allNodes.Where(n => n != layoverNode).ToList();
+
+            while (allRemainingNodes.Any())
+            //foreach (var layoverNode in minimalPathsFromNode.Keys)
+            {
+                var costToLayoverNode = minimalPathsFromNode[layoverNode];
+
+                foreach (var node in allNodes.Where(n => n != selectedNode && n != layoverNode))
+                {
+                    if (!CheckIfDirectPathExists(layoverNode, node))
+                    {
+                        continue;
+                    }
+
+                    var costFromSelectedNodeToNode = minimalPathsFromNode[node];
+                    var costFromLayoverNodeToNode = GetDirectPathCost(layoverNode, node);
+
+                    if (costFromLayoverNodeToNode + costToLayoverNode < costFromSelectedNodeToNode)
+                    {
+                        minimalPathsFromNode[node] = costFromLayoverNodeToNode + costToLayoverNode;
+                    }
+                }
+
+                allRemainingNodes.Remove(layoverNode);
+
+                if (allRemainingNodes.Any())
+                { 
+                    currentMinimalCost = minimalPathsFromNode.Where(n => allRemainingNodes.Contains(n.Key)).Min(n => n.Value);
+                    layoverNode = minimalPathsFromNode.Where(n => allRemainingNodes.Contains(n.Key) && n.Value == currentMinimalCost).Select(n => n.Key).FirstOrDefault();
+                }
+            }
+
+            return minimalPathsFromNode;
+        }
+
+        private bool CheckIfDirectPathExists(char startNode, char endNode)
+        {
+            var allPathsInBothDirections = new List<(char startNode, char endNode, int cost)>();
+
+            foreach (var path in givenPaths)
+            {
+                allPathsInBothDirections.Add((path.startNode, path.endNode, path.cost));
+                allPathsInBothDirections.Add((path.endNode, path.startNode, path.cost));
+            }
+
+            return allPathsInBothDirections.Exists(p => p.startNode == startNode && p.endNode == endNode);
+        }
+
+        private int GetDirectPathCost(char startNode, char endNode)
+        {
+            if (!CheckIfDirectPathExists(startNode, endNode))
+            {
+                return int.MaxValue;
+            }
+
+            var allPathsInBothDirections = new List<(char startNode, char endNode, int cost)>();
+
+            foreach (var path in givenPaths)
+            {
+                allPathsInBothDirections.Add((path.startNode, path.endNode, path.cost));
+                allPathsInBothDirections.Add((path.endNode, path.startNode, path.cost));
+            }
+
+            return allPathsInBothDirections.First(p => p.startNode == startNode && p.endNode == endNode).cost;
+        }
+    }
+}
---
+++ DijkstraAlgorithm/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace DijkstraAlgorithm
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var givenPaths = new List<(char startNode, char endNode, int cost)>
+            {
+                ('A', 'B', 2),
+                ('A', 'C', 4),
+                ('B', 'C', 2),
+                ('C', 'D', 1),
+                ('B', 'E', 10),
+                ('D', 'E', 4),
+                ('B', 'D', 4)
+            };
+
+            var graphWithNodes = new GraphWithNodes(givenPaths);
+
+            var minimalCostsFromNodeA = graphWithNodes.CalculateMinimalCostFromNode('A');
+            PrintResult(minimalCostsFromNodeA);
+            Console.WriteLine();
+
+            var moreGivenPaths = new List<(char startNode, char endNode, int cost)>
+            {
+                ('A', 'B', 2),
+                ('A', 'E', 7),
+                ('A', 'F', 2),
+                ('B', 'C', 2),
+                ('E', 'C', 1),
+                ('F', 'G', 1),
+                ('C', 'E', 1),
+                ('C', 'D', 3),
+                ('C', 'G', 1),
+                ('D', 'H', 1)
+            };
+
+            var anotherGraphWithNodes = new GraphWithNodes(moreGivenPaths);
+
+            var minimalCostsFromNodeAForNewGraph = anotherGraphWithNodes.CalculateMinimalCostFromNode('A');
+            PrintResult(minimalCostsFromNodeAForNewGraph);
+        }
+
+        public static void PrintResult(Dictionary<char, int> costs)
+        {
+            foreach (var cost in costs.OrderBy(c => c.Key))
+            {
+                Console.WriteLine(cost.Key + ": " + cost.Value);
+            }
+        }
+    }
+}
